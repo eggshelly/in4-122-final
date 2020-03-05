b82ec77e28ad6bb25f9fd150792b58428b1f0b32
Amanda Lin <amandalincs@gmail.com>
2020-03-04 20:17:48 -0800

    Added preliminary Columns functionality

--- team5.games/src/TestObj2.java
+++ team5.games/src/TestObj2.java
@@ -2,4 +2,10 @@ public class TestObj2 {
     public void print(){
         System.out.println("print from games mod");
     }
+
+    public static void main(String[] args)
+    {
+        Columns c = new Columns();
+        c.printBoard();
+    }
 }
