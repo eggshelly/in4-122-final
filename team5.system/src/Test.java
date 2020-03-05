aefb6cfb6199cb8c107b0d62ea4345e76ee5b334
Michelle Wang <zhiyimichelle@gmail.com>
2020-03-04 19:28:57 -0800

    finished modules (hopefully)

--- team5.system/src/Test.java
+++ team5.system/src/Test.java
@@ -2,6 +2,10 @@ public class Test {
 
     public static void  main(String[] args){
         System.out.println("aAAA");
+        TestObj2 t2 = new TestObj2();
+        t2.print();
+        TestObj t1 = new TestObj();
+        t1.print();
 
     }
 
