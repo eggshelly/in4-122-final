f481ff6b55a6b505776931bf0fa00484c1a15501
Jeimeil <jeimeilm@uci.edu>
2020-03-05 13:10:59 -0800

    Created MainMenu File

---
+++ team5.system/src/MainMenu.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Game {
+    // Dummy Class just so types can be there.
+}
+
+public class MainMenu {
+    public ArrayList<Game> gameList;
+    // GUI_Window
+    private int numPlayers;
+    private ArrayList<Player> playerList;
+
+    public void printMenu() {
+        // Print out Menu Text
+    }
+
+    
+}
