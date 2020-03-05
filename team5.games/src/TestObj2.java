5ed584ebd51c2ba1edfc1f97f1e7b0de8c92b6a4
Michelle Wang <zhiyimichelle@gmail.com>
2020-03-04 19:37:47 -0800

    modified modules

--- team5.games/src/TestObj2.java
+++ team5.games/src/TestObj2.java
@@ -2,4 +2,12 @@ public class TestObj2 {
     public void print(){
         System.out.println("print from games mod");
     }
+
+    public static void  main(String[] args){
+        System.out.println("aAAA");
+        TestObj t1 = new TestObj();
+        t1.print();
+        Test3 t = new Test3();
+        t.print();
+    }
 }
