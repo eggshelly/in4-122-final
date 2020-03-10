0299a247a6cc4dac26cd53ed91609c673b143dd3
efballes <efballes@uci.edu>
2020-03-09 19:23:06 -0700

    Game Manager moved over
    asdfsdoifhsghsdzhfjsf

--- team5.system/src/GameManager.java
+++ team5.system/src/GameManager.java
@@ -1,3 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class GameManager {
-    
+    private final static String PlayerFile = "UserNames.txt";
+    private ArrayList<Player> Players;
+
+
+    private void loadPlayers() {
+        String[] info;
+        try {
+            String line = "";
+            FileReader reader = new FileReader(PlayerFile);
+            Scanner in = new Scanner(reader);
+            Players.clear();
+            while (in.hasNextLine()) {
+                line = in.nextLine();
+                info = line.split(",", 2);
+                String userName = info[0];
+                int numGames = Integer.parseInt(info[1]);
+                Players.add(new Player(userName, numGames));
+            }
+            in.close();
+        } catch (Exception e) {
+
+        }
+    }
 }
