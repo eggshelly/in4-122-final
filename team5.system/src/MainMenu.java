8984830e370af4d080901eb9107f6b841826f139
Amanda Lin <amandalincs@gmail.com>
2020-03-11 16:34:20 -0700

    Added ColumnsGUI functionality to MainMenu. Will update this

--- team5.system/src/MainMenu.java
+++ team5.system/src/MainMenu.java
@@ -9,6 +9,8 @@ public class MainMenu {
     public static String[] games = new String[]{"Columns", "Bejeweled"};
     private static String[] players = new String[2];
     private static int numPlayers = 0;
+//    private static Game game; <- CHANGE TO THIS AFTER IMPLEMENTING COLUMNS AS GAME
+    private static ColumnsGUI game; // TEMP!
 
     public MainMenu() {
     }
@@ -30,14 +32,14 @@ public class MainMenu {
         chosenGame = scan.nextLine();
         boolean validGame = false;
 
-        String game;
+        String gameStr;
         while(!validGame) {
             String[] var7;
             int var6 = (var7 = games).length;
 
             for(int var10 = 0; var10 < var6; ++var10) {
-                game = var7[var10];
-                if (chosenGame.equals(game)) {
+                gameStr = var7[var10];
+                if (chosenGame.equals(gameStr)) {
                     validGame = true;
                 }
             }
@@ -52,14 +54,16 @@ public class MainMenu {
         System.out.println("You have chosen " + chosenGame);
         System.out.println("Enter your profile name.");
         System.out.print("Name: ");
-        game = scan.nextLine();
-        players[0] = game;
+        gameStr = scan.nextLine();
+        players[0] = gameStr;
         System.out.println("Will you have another player? Enter the name if yes. Otherwise, leave it blank and press enter.");
         System.out.print("Name: ");
         String secondPlayer = scan.nextLine();
         if (secondPlayer.equals("")) {
             System.out.println("Starting a single player game.");
             numPlayers = 1;
+            game = new ColumnsGUI();
+            game.run();
         } else {
             System.out.println(secondPlayer + " has joined. Starting two-player game.");
             players[1] = secondPlayer;
