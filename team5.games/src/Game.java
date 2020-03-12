6b707bf0da604f0c69b4e8e97d33775eabb941f1
Amanda Lin <amandalincs@gmail.com>
2020-03-11 20:10:16 -0700

    Changed Game to abstract class. Adjusted Columns to fit structure

--- team5.games/src/Game.java
+++ team5.games/src/Game.java
@@ -1,10 +1,59 @@
+import java.util.ArrayList;
 
-public interface Game {
-	public void checkMatch();
-	public void run();
-	public void makeBoard();
-	public void makeMove();
-	public void checkMove();
-	public void printBoard();
-	public void generatePiece();
+public abstract class Game {
+	public int rows;
+	public int cols;
+	public int[][] board;
+	public ArrayList<ArrayList<Integer>> matched;
+	public static final int EMPTY = 0;
+	public int score;
+
+	public void initializeBoard()
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				board[i][j] = EMPTY;
+			}
+		}
+	}
+
+	public int getCell(int row, int col)
+	{
+		return board[row][col];
+	}
+
+	public int getRows()
+	{
+		return rows;
+	}
+
+	public int getCols()
+	{
+		return cols;
+	}
+
+	public int getNumMatched()
+	{
+		return matched.size();
+	}
+
+	public ArrayList<ArrayList<Integer>> getMatched()
+	{
+		return matched;
+	}
+
+	public int getScore()
+	{
+		return score;
+	}
+
+	public abstract void checkMatch();
+	public abstract void run();
+	public abstract void printBoard();
+
+//	public abstract void makeMove();
+//	public abstract void checkMove();
+//	public abstract void generatePiece();
 }
